ff0388239519857ec39cbce5628dcae90068c1f7
vũ <vanvux99@gmail.com>
2021-01-30 08:42:34 +0700

    ok

---
+++ blog.js
@@ -0,0 +1,572 @@
+var dataJson = {
+    "users": [{
+            "id": 1,
+            "name": "Tiger Nixon",
+            "position": 1,
+            "salary": "320800",
+            "start_date": "2011-04-25",
+            "office": 1,
+            "extn": "5421"
+        },
+        {
+            "id": 2,
+            "name": "Garrett Winters",
+            "position": 2,
+            "salary": "170750",
+            "start_date": "2011-07-25",
+            "office": 2,
+            "extn": "8422"
+        },
+        {
+            "id": 3,
+            "name": "Ashton Cox",
+            "position": 3,
+            "salary": "86000",
+            "start_date": "2009-01-12",
+            "office": 1,
+            "extn": "1562"
+        },
+        {
+            "id": 4,
+            "name": "Cedric Kelly",
+            "position": 4,
+            "salary": "433060",
+            "start_date": "2012-03-29",
+            "office": 3,
+            "extn": "6224"
+        },
+        {
+            "id": 5,
+            "name": "Airi Satou",
+            "position": 3,
+            "salary": "162700",
+            "start_date": "2008-11-28",
+            "office": 4,
+            "extn": "5407"
+        },
+        {
+            "id": 6,
+            "name": "Brielle Williamson",
+            "position": 2,
+            "salary": "372000",
+            "start_date": "2012-12-02",
+            "office": 1,
+            "extn": "4804"
+        },
+        {
+            "id": 7,
+            "name": "Herrod Chandler",
+            "position": 4,
+            "salary": "137500",
+            "start_date": "2012-08-06",
+            "office": 2,
+            "extn": "9608"
+        },
+        {
+            "id": 8,
+            "name": "Rhona Davidson",
+            "position": 1,
+            "salary": "327900",
+            "start_date": "2010-10-14",
+            "office": 3,
+            "extn": "6200"
+        },
+        {
+            "id": 9,
+            "name": "Colleen Hurst",
+            "position": 2,
+            "salary": "205500",
+            "start_date": "2009-09-15",
+            "office": 1,
+            "extn": "2360"
+        },
+        {
+            "id": 10,
+            "name": "Sonya Frost",
+            "position": 4,
+            "salary": "103600",
+            "start_date": "2008-12-13",
+            "office": 3,
+            "extn": "1667"
+        },
+        {
+            "id": 11,
+            "name": "Office Manager",
+            "position": 3,
+            "salary": "90560",
+            "start_date": "2008-12-19",
+            "office": 5,
+            "extn": "3814"
+        },
+        {
+            "id": 12,
+            "name": "Quinn Flynn",
+            "position": 3,
+            "salary": "342000",
+            "start_date": "2013-03-03",
+            "office": 3,
+            "extn": "9497"
+        },
+        {
+            "id": 55,
+            "name": "Regional Director",
+            "position": 2,
+            "salary": "470600",
+            "start_date": "2008-10-16",
+            "office": 1,
+            "extn": "6741"
+        },
+        {
+            "id": 13,
+            "name": "Haley Kennedy",
+            "position": 4,
+            "salary": "313500",
+            "start_date": "2012-12-18",
+            "office": 5,
+            "extn": "3597"
+        },
+        {
+            "id": 54,
+            "name": "Tatyana Fitzpatrick",
+            "position": 1,
+            "salary": "385750",
+            "start_date": "2010-03-17",
+            "office": 5,
+            "extn": "1965"
+        },
+        {
+            "id": 14,
+            "name": "Michael Silva",
+            "position": 2,
+            "salary": "198500",
+            "start_date": "2012-11-27",
+            "office": 5,
+            "extn": "1581"
+        },
+        {
+            "id": 15,
+            "name": "Paul Byrd",
+            "position": 1,
+            "salary": "725000",
+            "start_date": "2010-06-09",
+            "office": 1,
+            "extn": "3059"
+        },
+        {
+            "id": 16,
+            "name": "Gloria Little",
+            "position": 1,
+            "salary": "237500",
+            "start_date": "2009-04-10",
+            "office": 1,
+            "extn": "1721"
+        },
+        {
+            "id": 17,
+            "name": "Bradley Greer",
+            "position": 2,
+            "salary": "132000",
+            "start_date": "2012-10-13",
+            "office": 5,
+            "extn": "2558"
+        },
+        {
+            "id": 18,
+            "name": "Dai Rios",
+            "position": 3,
+            "salary": "217500",
+            "start_date": "2012-09-26",
+            "office": 3,
+            "extn": "2290"
+        },
+        {
+            "id": 19,
+            "name": "Jenette Caldwell",
+            "position": 4,
+            "salary": "345000",
+            "start_date": "2011-09-03",
+            "office": 1,
+            "extn": "1937"
+        },
+        {
+            "id": 20,
+            "name": "Yuri Berry",
+            "position": 4,
+            "salary": "675000",
+            "start_date": "2009-06-25",
+            "office": 1,
+            "extn": "6154"
+        },
+        {
+            "id": 21,
+            "name": "Caesar Vance",
+            "position": 4,
+            "salary": "106450",
+            "start_date": "2011-12-12",
+            "office": 1,
+            "extn": "8330"
+        },
+        {
+            "id": 22,
+            "name": "Doris Wilder",
+            "position": 1,
+            "salary": "85600",
+            "start_date": "2010-09-20",
+            "office": 2,
+            "extn": "3023"
+        },
+        {
+            "id": 23,
+            "name": "Angelica Ramos",
+            "position": 3,
+            "salary": "1200000",
+            "start_date": "2009-10-09",
+            "office": 5,
+            "extn": "5797"
+        },
+        {
+            "id": 24,
+            "name": "Gavin Joyce",
+            "position": 2,
+            "salary": "92575",
+            "start_date": "2010-12-22",
+            "office": 3,
+            "extn": "8822"
+        },
+        {
+            "id": 25,
+            "name": "Jennifer Chang",
+            "position": 2,
+            "salary": "357650",
+            "start_date": "2010-11-14",
+            "office": 4,
+            "extn": "9239"
+        },
+        {
+            "id": 26,
+            "name": "Brenden Wagner",
+            "position": 3,
+            "salary": "206850",
+            "start_date": "2011-06-07",
+            "office": 1,
+            "extn": "1314"
+        },
+        {
+            "id": 27,
+            "name": "Fiona Green",
+            "position": 2,
+            "salary": "850000",
+            "start_date": "2010-03-11",
+            "office": 1,
+            "extn": "2947"
+        },
+        {
+            "id": 28,
+            "name": "Shou Itou",
+            "position": 1,
+            "salary": "163000",
+            "start_date": "2011-08-14",
+            "office": 3,
+            "extn": "8899"
+        },
+        {
+            "id": 29,
+            "name": "Michelle House",
+            "position": 2,
+            "salary": "95400",
+            "start_date": "2011-06-02",
+            "office": 2,
+            "extn": "2769"
+        },
+        {
+            "id": 30,
+            "name": "Suki Burks",
+            "position": 2,
+            "salary": "114500",
+            "start_date": "2009-10-22",
+            "office": 5,
+            "extn": "6832"
+        },
+        {
+            "id": 53,
+            "name": "Prescott Bartlett",
+            "position": 3,
+            "salary": "145000",
+            "start_date": "2011-05-07",
+            "office": 5,
+            "extn": "3606"
+        },
+        {
+            "id": 31,
+            "name": "Gavin Cortez",
+            "position": 4,
+            "salary": "235500",
+            "start_date": "2008-10-26",
+            "office": 1,
+            "extn": "2860"
+        },
+        {
+            "id": 32,
+            "name": "Martena Mccray",
+            "position": 4,
+            "salary": "324050",
+            "start_date": "2011-03-09",
+            "office": 3,
+            "extn": "8240"
+        },
+        {
+            "id": 33,
+            "name": "Unity Butler",
+            "position": 1,
+            "salary": "85675",
+            "start_date": "2009-12-09",
+            "office": 1,
+            "extn": "5384"
+        },
+        {
+            "id": 34,
+            "name": "Howard Hatfield",
+            "position": 4,
+            "salary": "164500",
+            "start_date": "2008-12-16",
+            "office": 1,
+            "extn": "7031"
+        },
+        {
+            "id": 56,
+            "name": "Hope Fuentes",
+            "position": 4,
+            "salary": "109850",
+            "start_date": "2010-02-12",
+            "office": 1,
+            "extn": "6318"
+        },
+        {
+            "id": 35,
+            "name": "Vivian Harrell",
+            "position": 2,
+            "salary": "452500",
+            "start_date": "2009-02-14",
+            "office": 1,
+            "extn": "9422"
+        },
+        {
+            "id": 57,
+            "name": "Timothy Mooney",
+            "position": 2,
+            "salary": "136200",
+            "start_date": "2008-12-11",
+            "office": 5,
+            "extn": "7580"
+        },
+        {
+            "id": 36,
+            "name": "Jackson Bradshaw",
+            "position": 2,
+            "salary": "645750",
+            "start_date": "2008-09-26",
+            "office": 1,
+            "extn": "1042"
+        },
+        {
+            "id": 37,
+            "name": "Olivia Liang",
+            "position": 4,
+            "salary": "234500",
+            "start_date": "2011-02-03",
+            "office": 4,
+            "extn": "2120"
+        },
+        {
+            "id": 38,
+            "name": "Bruno Nash",
+            "position": 4,
+            "salary": "163500",
+            "start_date": "2011-05-03",
+            "office": 5,
+            "extn": "6222"
+        },
+        {
+            "id": 39,
+            "name": "Sakura Yamamoto",
+            "position": 1,
+            "salary": "139575",
+            "start_date": "2009-08-19",
+            "office": 3,
+            "extn": "9383"
+        },
+        {
+            "id": 40,
+            "name": "Thor Walton",
+            "position": 1,
+            "salary": "98540",
+            "start_date": "2013-08-11",
+            "office": 1,
+            "extn": "8327"
+        },
+        {
+            "id": 41,
+            "name": "Finn Camacho",
+            "position": 2,
+            "salary": "87500",
+            "start_date": "2009-07-07",
+            "office": 1,
+            "extn": "2927"
+        },
+        {
+            "id": 41,
+            "name": "Serge Baldwin",
+            "position": 2,
+            "salary": "138575",
+            "start_date": "2012-04-09",
+            "office": 4,
+            "extn": "8352"
+        },
+        {
+            "id": 43,
+            "name": "Zenaida Frank",
+            "position": 3,
+            "salary": "125250",
+            "start_date": "2010-01-04",
+            "office": 1,
+            "extn": "7439"
+        },
+        {
+            "id": 44,
+            "name": "Zorita Serrano",
+            "position": 4,
+            "salary": "115000",
+            "start_date": "2012-06-01",
+            "office": 1,
+            "extn": "4389"
+        },
+        {
+            "id": 45,
+            "name": "Jennifer Acosta",
+            "position": 1,
+            "salary": "75650",
+            "start_date": "2013-02-01",
+            "office": 3,
+            "extn": "3431"
+        },
+        {
+            "id": 46,
+            "name": "Cara Stevens",
+            "position": 1,
+            "salary": "145600",
+            "start_date": "2011-12-06",
+            "office": 1,
+            "extn": "3990"
+        },
+        {
+            "id": 47,
+            "name": "Hermione Butler",
+            "position": 1,
+            "salary": "356250",
+            "start_date": "2011-03-21",
+            "office": 5,
+            "extn": "1016"
+        },
+        {
+            "id": 48,
+            "name": "Lael Greer",
+            "position": 2,
+            "salary": "103500",
+            "start_date": "2009-02-27",
+            "office": 5,
+            "extn": "6733"
+        },
+        {
+            "id": 49,
+            "name": "Jonas Alexander",
+            "position": 3,
+            "salary": "86500",
+            "start_date": "2010-07-14",
+            "office": 1,
+            "extn": "8196"
+        },
+        {
+            "id": 50,
+            "name": "Shad Decker",
+            "position": 3,
+            "salary": "183000",
+            "start_date": "2008-11-13",
+            "office": 3,
+            "extn": "6373"
+        },
+        {
+            "id": 51,
+            "name": "Michael Bruce",
+            "position": 2,
+            "salary": "183000",
+            "start_date": "2011-06-27",
+            "office": 4,
+            "extn": "5384"
+        },
+        {
+            "id": 52,
+            "name": "Donna Snider",
+            "position": 4,
+            "salary": "112000",
+            "start_date": "2011-01-25",
+            "office": 1,
+            "extn": "4226"
+        }
+    ],
+    "positions": [{
+            "id": 1,
+            "name": "System Architect"
+        },
+        {
+            "id": 2,
+            "name": "Customer Support"
+        },
+        {
+            "id": 3,
+            "name": "Javascript Developer"
+        },
+        {
+            "id": 4,
+            "name": "Regional Director"
+        }
+    ],
+    "offices": [{
+            "id": 1,
+            "name": "New York"
+        },
+        {
+            "id": 2,
+            "name": "Argentina"
+        },
+        {
+            "id": 3,
+            "name": "Edinburgh"
+        },
+        {
+            "id": 4,
+            "name": "San Francisco"
+        },
+        {
+            "id": 5,
+            "name": "London"
+        }
+    ]
+}
+
+// $('#table3').DataTable({
+//     data: dataJson.users,
+//     columns: [
+//         { "data ": 'id' },
+//         { "data ": 'name' },
+//         { "data ": 'position' },
+//         { "data ": 'salary' },
+//         { "data ": 'start_date' },
+//         { "data ": 'office' },
+//         { "data ": 'extn' },
+//         {
+//             "render ": function() {
+//                 return '<button type="button " class="btn btn-danger delete ">Xóa</button>'
+//             }
+//         }
+//     ]
+// });
